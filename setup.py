7c5fc71b06df35ae61e0c8038e0780b132f3768c
Jörg Thalheim <joerg@thalheim.io>
2018-10-05 19:51:47 +0100

    specify dependencies correctly

--- setup.py
+++ setup.py
@@ -13,13 +13,12 @@ setup(
     version="1.0.0",
     classifiers=CLASSIFIERS,
     description="A bot posting HN-Articles to mastodon",
+    install_requires=["feedparser", "Mastodon.py"],
     author="Joerg Thalheim",
     author_email="joerg@thalheim.io",
     url="https://github.com/Mic92/mastodon-hnbot",
     license="MIT License",
     py_modules=["hnbot"],
-    entry_points={
-        "console_scripts": ["hnbot = hnbot:main"]
-    },
+    entry_points={"console_scripts": ["hnbot = hnbot:main"]},
     zip_safe=True,
 )
